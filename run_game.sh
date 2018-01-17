83d431ea1041fba18f55ad13e09c5bc91c406f4c
Alex <alexycodes@gmail.com>
2018-01-16 23:33:41 -0800

    Revert "let's just use MyBot instead of alexy3"
    This reverts commit ffce3d221172b06bf55353f3c821200ce3ef1526.

--- run_game.sh
+++ run_game.sh
@@ -5,9 +5,9 @@ set -e
 SYSTEM=`uname -s`
 
 if [ "${SYSTEM}" == "Linux" ]; then
-    ./halite.linux -d "240 160" "python3 settler.py" "python3 MyBot.py"
+    ./halite.linux -d "240 160" "python3 settler.py" "python3 alexy3.py"
 elif [ "${SYSTEM}" == "Darwin" ]; then
-    ./halite.macos -d "240 160" "python3 settler.py" "python3 MyBot.py"
+    ./halite.macos -d "240 160" "python3 settler.py" "python3 alexy3.py"
 else
     echo "No such executable for system ${SYSTEM} found."
     exit 1;
